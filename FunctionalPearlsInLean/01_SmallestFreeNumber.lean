6b57ada9663b9d4433ab2f7f41b895dee537448e
Michiel Huttener <mutten-lambda@protonmail.com>
2025-06-27 23:55:04 +0200

    add explanation first pearld

---
+++ FunctionalPearlsInLean/01_SmallestFreeNumber.lean
@@ -0,0 +1,15 @@
+/-
+Problem:
+Compute the smallest natural number not in a given finite set X
+of natural numbers.
+
+Note:
+The third solution uses the fact that any number occurs at most once.
+To compare all algorithms, this will be assumed for all of them.
+-/
+
+-- Naive solution
+
+-- Array based solution
+
+-- Divide and conquer solution
